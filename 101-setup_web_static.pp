4e1855ef7977a5159e10bccb3d09136ebbf9c485
torres1-23 <alejo.torres1247@gmail.com>
2021-04-16 00:06:52 +0000

    Add puppet script

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -21,11 +21,21 @@ package { 'nginx':
   provider => 'apt'
 }
 
+-> file { '/data':
+  ensure  => 'directory'
+}
+
+-> file { '/data/web_static':
+  ensure  => 'directory'
+}
 -> file { '/data/web_static/shared':
   ensure => directory
 }
 
-# create direcotries /data/web_static/releases/
+-> file { '/data/web_static/releases':
+  ensure => directory
+}
+
 -> file { '/data/web_static/releases/test':
   ensure => directory
 }
