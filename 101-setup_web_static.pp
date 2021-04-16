d1eae44312d0fd724b13d2f28cc3fbd6d128b3ee
torres1-23 <alejo.torres1247@gmail.com>
2021-04-16 00:33:02 +0000

    Add puppet script

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -59,6 +59,10 @@ package { 'nginx':
   content => $conf
 }
 
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
 -> file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
