dd2f40ddb35f8f0d7a1f3d2abd52d093c050cc09
torres1-23 <alejo.torres1247@gmail.com>
2021-04-30 01:18:01 +0000

    Add script to display a sting in web server on root URL and location URL

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application.
+Usage:
+    Run this script with: 'python3 -m 0-hello_route'
+    to start a web server running on 'http://0.0.0.0:5000/'"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays string.
+
+    Returns:
+        Return string 'Hello HBNB'
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays string.
+
+    Returns:
+        Return string 'HBNB'
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
