598ceb628a73703abaf122ff3c8e11b64a68f38b
torres1-23 <alejo.torres1247@gmail.com>
2021-05-02 17:48:57 +0000

    Add web app to list all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+
+Usage :
+    Execute it like this: "python3 -m web_flask.7-states_list"
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page.
+
+    Return:
+        HTML page.
+    """
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
